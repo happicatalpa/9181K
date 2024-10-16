498216537fa34ee4440b81d0a3a00bea86e65886
pixilate <ginatyliu@gmail.com>
2024-10-16 00:20:31 -0700

    changed direction of OP controls

--- src/main.cpp
+++ src/main.cpp
@@ -87,8 +87,9 @@ void opcontrol() {
 		// Arcade control scheme
 		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
 		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
+		left_mg.move(dir + turn);                      // Sets left motor voltage
+		right_mg.move(dir - turn);                     // Sets right motor voltage
+
 		pros::delay(20);                               // Run for 20 ms then update
 	}
 }
